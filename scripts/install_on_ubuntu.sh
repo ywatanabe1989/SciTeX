171f7a3ea851038c00c87124db18aca652a7b49c
Yusuke Watanabe <wyusuuke@gmail.com>
2024-01-10 18:43:27 +1100

    update

--- scripts/install_on_ubuntu.sh
+++ scripts/install_on_ubuntu.sh
@@ -5,5 +5,6 @@ sudo apt-get install texlive-full -y
 sudo apt-get install tree -y
 sudo apt-get install bibtex -y
 sudo apt-get install xlsx2csv csv2latex -y
+sudo apt-get install diffpdf -y
 
 ## EOF
