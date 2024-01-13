78d0127b75d04bc828547b48993d7f01adb27da8
Yusuke Watanabe <wyusuuke@gmail.com>
2024-01-13 21:33:53 +1100

    v045

--- .scripts/sh/install_on_ubuntu.sh
+++ .scripts/sh/install_on_ubuntu.sh
@@ -6,7 +6,6 @@ sudo apt-get install tree -y
 sudo apt-get install bibtex -y
 sudo apt-get install xlsx2csv csv2latex -y
 # sudo apt-get install diffpdf -y
-sudo apt-get install yes -y
 
 
 # ./.scripts/sh/install_on_ubuntu.sh
