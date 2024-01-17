0522a110a3b41b12e67919f9ce418afb78b74ac0
Yusuke Watanabe <wyusuuke@gmail.com>
2024-01-17 11:39:17 +1100

    submission

--- .scripts/sh/.pptx2tif.sh
+++ .scripts/sh/.pptx2tif.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-inputFilePath=$1
+inputFilePath=$1 # must be fullpath
+
 echo -e "\nConverting ${inputFilePath}..."
 
 inputFilePathWin=$(wslpath -w "$inputFilePath")
 
 if [ "$#" -eq 2 ]; then
     outputFilePath=$2
-    outputFilePathWin=$(wslpath -w "$outputFilePath")    
+    outputFilePathWin=$(wslpath -w "$outputFilePath")
 else
     outputFilePathWin=${inputFilePathWin%.pptx}.tif
 fi
@@ -15,10 +16,11 @@ fi
 # Run the PowerShell script
 powershell.exe -ExecutionPolicy Bypass -File "$(wslpath -w ./.scripts/ps1/pptx2tiff.ps1)" -inputFilePath "$inputFilePathWin" -outputFilePath "$outputFilePathWin"
 
+
 if [ -n "$inputFilePath" ]; then
    echo -e "\nConverted: ${inputFilePath} -> ${inputFilePath%.pptx}.tif"
 fi
 
-# ./.scripts/sh/.pptx2tif.sh src/figures/Figure_ID_04.pptx
+# ./.scripts/sh/.pptx2tif.sh src/figures/Figure_ID_01.pptx
 
 ## EOF
